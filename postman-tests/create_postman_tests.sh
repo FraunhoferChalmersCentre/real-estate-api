4ce858a25632be1c2d3d7f3b5092c722d1d2b46a
Adrian Nilsson <adnilsson@users.noreply.github.com>
2021-09-06 13:41:25 +0200

    Move Postman related variables
    I noticed that portman can inject Postman variables from the user's
    ENV-variables.
    This works well with the containerized portman wrapper.
    Also, we can remove the variables from the API specification since
    they're only used when generating Postman tests.

--- postman-tests/create_postman_tests.sh
+++ postman-tests/create_postman_tests.sh
@@ -3,5 +3,9 @@
 IMAGE_TAG=${IMAGE_TAG:=fcc-sys/oas-to-postman}
 
 docker run --rm \
+    -e PORTMAN_TEST_HEATING_SYSTEM_ID=aHeatingSystemId \
+    -e PORTMAN_TEST_RADIATOR_LOOP_ID=aRadiatorLoopId \
+    -e PORTMAN_TEST_BUILDING_ID=aBuildingId \
+    -e PORTMAN_TEST_SENSOR_ID=aSensorId \
     --mount type=bind,source=$PWD,target=/mnt/host \
     $IMAGE_TAG $@
